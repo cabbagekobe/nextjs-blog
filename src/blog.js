2aa1edfdbc5d9ad473ca5dd7c1eb7c7969ccf34b
cabbagekobe <circlekdeath@gmail.com>
2021-01-23 00:56:30 +0900

    TailwindcssBlogに変更

---
+++ src/blog.js
@@ -0,0 +1,9 @@
+export const Blog = {
+  author: "キャベツこうべ",
+  title: "cabbagekobe.info",
+  language: "ja",
+
+  github: "cabbagekobe",
+  twitter: "cabbagekobe",
+  domain: "cabbagekobe.info",
+}
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-10">
+      <div>
+        <Link href="/">
+          <a aria-label="Tailwind CSS Blog">
+            cabbagekobe.info
+          </a>
+        </Link>
+      </div>
+
+      <div className="text-base leading-5">
+        <Link href="/pages/issue">
+          <a className="font-medium text-gray-500 hover:text-gray-700" aria-label="Tailwind CSS Blog">
+            Documentation &rarr;
+          </a>
+        </Link>
+      </div>
+    </header>
+  )
+}
