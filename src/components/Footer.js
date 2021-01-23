50a573c062bed883008566b63dff27c67dbd5661
cabbagekobe <circlekdeath@gmail.com>
2021-01-24 01:41:18 +0900

    フッターの追加

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="py-10">
+      <p className="text-center">
+        <a href="https://twitter.com/cabbagekobe">
+          &copy;cabbagekobe.info
+        </a>
+      </p>
+    </footer>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/css/tailwind.css";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import SectionContainer from "@/components/SectionContainer";
 
 export default function App({ Component, pageProps }) {
@@ -27,6 +28,10 @@ export default function App({ Component, pageProps }) {
         <main>
           <Component {...pageProps} />
         </main>
+      </SectionContainer>
+
+      <SectionContainer>
+        <Footer />
       </SectionContainer>
     </div>
   );
