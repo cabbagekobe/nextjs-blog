cda5b119e6d4e72a843635312578e2b25ecf03b6
cabbagekobe <circlekdeath@gmail.com>
2021-02-08 16:50:09 +0900

    ライブラリアップデート

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,14 +12,6 @@ export default function Header() {
           </a>
         </Link>
       </h1>
-
-      <div className="text-base leading-5">
-        <Link href="/pages/issue">
-          <a className="text-gray-500 hover:text-gray-700" aria-label="Tailwind CSS Blog">
-            Documentation &rarr;
-          </a>
-        </Link>
-      </div>
     </header>
   );
 }
